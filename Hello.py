dfa027a26ce551e2ede5781a4fb07b896a22a6c5
Nikita Jalkote <53075857+nikitajalkote@users.noreply.github.com>
2024-05-06 01:56:21 +0530

    adding simple python file

---
+++ Hello.py
@@ -0,0 +1,17 @@
+print("Hello, world!")
+variable = "Python"
+variabl1 = 3.12
+print("programming in", variable, variabl1)
+python=True
+name = "Nikita"
+age = 27
+print(f"Name:{name}, AGE:{age}")
+#f string formatting
+"""This is 
+multiline 
+comment"""
+
+print("""
+This is multiline String
+which is used in three double quotes
+      """)
